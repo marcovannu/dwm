ac12dbdbdfcb72b15d64b12672bf17772fa1f92f
marcovannu <marcovannucci99@gmail.com>
2020-04-25 12:02:49 +0200

    config

--- config.def.h
+++ config.def.h
@@ -8,9 +8,9 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=8" };
 static const char dmenufont[]       = "monospace:size=8";
 static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray2[]       = "#a89984";
+static const char col_gray3[]       = "#a89984";
+static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#689D6A";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
