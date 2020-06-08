1e656ece0182d74efba06db70c7256d1ca7121a5
marcovannu <marcovannucci99@gmail.com>
2020-06-08 15:13:33 +0200

    swallow

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=8" };
@@ -27,12 +28,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 4,           0,           -1 },
-	{ "Darktable",     NULL,       NULL,       1 << 4,           0,           -1 },
-	{ "TelegramDesktop",   NULL,       NULL,       1 << 6,           0,           -1 },
-        { "Transmission-gtk",    NULL,       NULL,       1 << 7,           0,           -1 },
-	/*{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },*/
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+        { "Darktable", NULL,   NULL,           1 << 4,    0,          0,           0,         -1 },
+        { "TelegramDesktop", NULL, NULL,       1 << 6,    0,          0,           0,         -1 },
+        { "Transmission-gtk", NULL, NULL,      1 << 7,    0,          0,           0,         -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
